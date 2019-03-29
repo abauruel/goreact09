83f2dbb9d7bf137c0f0f780ba3e2e444aa2f7646
Alex Claude <abauruel@gmail.com>
2019-03-28 23:48:07 -0300

    Adicionado player

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import Global from './styles/global';
 
 import Sidebar from './components/Sidebar';
+import Player from './components/Player';
+
+import { Wrapper, Container } from './styles/components';
 
 const App = () => (
   <div>
+    <Wrapper>
+      <Container>
+        <Sidebar />
+      </Container>
+      <Player />
+    </Wrapper>
+
     <Global />
-    <Sidebar />
   </div>
 );
 
---
+++ src/components/Player/index.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Slider from 'rc-slider';
+import {
+  Container, Current, Volume, Controls, Progress, ProgressStyle, Time,
+} from './styles';
+
+import VolumeIcon from '../../assets/images/volume.svg';
+
+import ShuffleIcon from '../../assets/images/shuffle.svg';
+import BackwardIcon from '../../assets/images/backward.svg';
+import PlayIcon from '../../assets/images/play.svg';
+import PauseIcon from '../../assets/images/pause.svg';
+import ForwardIcon from '../../assets/images/forward.svg';
+import RepeatIcon from '../../assets/images/repeat.svg';
+
+const Player = () => (
+  <Container>
+    <Current>
+      <img
+        src="http://designontherocks.blog.br/wp-content/uploads/2010/07/typoster2.jpg"
+        alt="cover"
+      />
+
+      <div>
+        <span>Dark side of the moon</span>
+        <small>Pink Floyd</small>
+      </div>
+    </Current>
+    <Progress>
+      <Controls>
+        <button>
+          <img src={ShuffleIcon} alt="Shurffle" />
+        </button>
+        <button>
+          <img src={BackwardIcon} alt="Backward" />
+        </button>
+        <button>
+          <img src={PlayIcon} alt="Play" />
+        </button>
+        <button>
+          <img src={ForwardIcon} alt="Forward" />
+        </button>
+        <button>
+          <img src={RepeatIcon} alt="Repeat" />
+        </button>
+      </Controls>
+
+      <Time>
+        <span>1:30</span>
+        <ProgressStyle>
+          <Slider
+            railStyle={{ background: '#404040', borderRadius: 10 }}
+            trackStyle={{ background: '#1ED760' }}
+            handleStyle={{ border: 0 }}
+          />
+        </ProgressStyle>
+        <span>5:00</span>
+      </Time>
+    </Progress>
+    <Volume>
+      <img src={VolumeIcon} alt="Volume" />
+      <Slider
+        railStyle={{ background: '#404040', borderRadius: 10 }}
+        trackStyle={{ background: '#FFF' }}
+        handleStyle={{ display: 'none' }}
+        value={100}
+      />
+    </Volume>
+  </Container>
+);
+
+export default Player;
